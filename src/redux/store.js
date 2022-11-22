4e2b44bd1e3d696b4498dade7cb4e3b4adab8647
molim <mohade3.karbalaei@gmail.com>
2022-11-22 18:28:20 +0100

    fetch song

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+// simple set up funstion:
+import { configureStore } from "@reduxjs/toolkit";
 
-import playerReducer from './features/playerSlice';
+import playerReducer from "./features/playerSlice";
 
 export const store = configureStore({
   reducer: {
     player: playerReducer,
   },
 });
+
+// focus on new functionality-> implement to add in redux store
+// to fetch our songs: we create new folder:
