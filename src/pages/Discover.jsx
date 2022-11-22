18ef82c4ac3b9003bb1676039d27ff0c05891810
molim <mohade3.karbalaei@gmail.com>
2022-11-22 19:24:16 +0100

    add choosing the genres

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,46 @@
-const Discover = () => <div>Discover</div>;
+// first page and main page:
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genreTitle = "Pop";
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        {/* 1- for choosing: */}
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5 "
+        >
+          {/* map from genres: */}
+          {/* each option has key and value */}
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* 2- wrapper for songs: display the songs:*/}
+      {/* from  API   */}
+      <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            // specific index:
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
+
+// we want to fetch data from shazam api:
