6fd939788e4a8bbad40200b191e9b5543b6a52d1
molim <mohade3.karbalaei@gmail.com>
2022-11-23 15:37:51 +0100

    add loader and img

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,13 @@
-const Loader = () => (
-  <div>Loader</div>
+//after adding isFetching and error we have to add loader and error components:
+import { loader } from "../assets";
+
+const Loader = ({ title }) => (
+  <div className="w-full justify-center flex flex-col items-center">
+    <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
+    <h1 className="font-bold text-2xl text-white mt-2">
+      {title || "Loading..."}
+    </h1>
+  </div>
 );
 
 export default Loader;
