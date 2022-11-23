7a96b100f25803f1c6a2d65d7e37d04f46fc60b9
molim <mohade3.karbalaei@gmail.com>
2022-11-23 17:12:12 +0100

    add useGetTopChartsQuery

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,26 @@
 // first page and main page:
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+//3- after adding the API: (shazamCore)
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  // we can call it(useGetTopChartsQuery => api) like other hooks:
+  // we get back 3 different things:
+  // a- data: the data we get back from the api => result of api call
+  // b- isFetching: boolean => if we are fetching data or not
+  // c- error: if there is an error
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
+
+  // 4- for lodaidng:
+  if (isFetching) {
+    return <Loader title="Loading Songs..." />;
+  }
+  if (error) {
+    return <Error title="Error" />;
+  }
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -28,7 +45,7 @@ const Discover = () => {
       {/* 2- wrapper for songs: display the songs:*/}
       {/* from  API   */}
       <div className="flex flex-wrap justify-center sm:justify-start gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
