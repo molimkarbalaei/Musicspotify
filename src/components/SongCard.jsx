806c9cdb7a6b54a7bc3ad235b51b0a6ef595585b
molim <mohade3.karbalaei@gmail.com>
2022-11-23 17:23:50 +0100

    add songcard+ coverpicture

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,34 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+//  we write it once and use it 50 times.
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+//player functionality:
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+
+// we pass index and key as props:
+
+const SongCard = ({ i, song }) => {
+  //hardcoded the active song:
+  const activeSong = "Test";
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer ">
+      <div className="relative w-full h-56 group ">
+        <div
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            // if the song is playing is the song is showing:
+            activeSong?.title === song.title
+              ? "flex bg-black bg-opacity-70"
+              : "hidden"
+          }`}
+        >
+          {/* play or pause the song: */}
+          <PlayPause />
+        </div>
+        <img alt="song_img" src={song.images?.coverart} />
+      </div>
+      {/* Add title and subtitle: */}
+    </div>
+  );
+};
 
 export default SongCard;
