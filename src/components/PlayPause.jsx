f548edcfd1c6ed7d14c17e44f7e4fcf75c72d591
molim <mohade3.karbalaei@gmail.com>
2022-11-24 17:12:29 +0100

    add FaPauseCircle and FalPlayCircle

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,11 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+//after adding songcards
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle className="text-gray-300" size={35} onClick={handlePause} />
+  ) : (
+    <FaPlayCircle onClick={handlePlay} size={35} className="text-gray-300" />
+  );
+// if we want that those handle work we have to go to the songcard
 export default PlayPause;
