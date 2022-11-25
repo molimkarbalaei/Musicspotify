a42cec6073e4d0f12251f8fefa4e86dcf782e958
molim <mohade3.karbalaei@gmail.com>
2022-11-25 19:27:56 +0100

    feat: add TopChartCard and map it

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -15,6 +15,13 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 // custom hook for getting data from api:
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
+// todo: create the TopChartCard component:(later)
+const TopChartCard = ({ song, i }) => (
+  <div className="w-full flex flex-row hover:bg-[#4c426e] items-center py-2 p-4 rounded-lg cursor-pointer mb-2">
+    {song.title}
+  </div>
+);
+
 const TopPlay = () => {
   // todo: initial some states:
   const dispatch = useDispatch();
@@ -52,7 +59,19 @@ const TopPlay = () => {
       ref={divRef}
       className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-full flex flex-col"
     >
-      asdfg
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts"></Link>
+          <p className="text-gray-300 text-base cursor-pointer">See More</p>
+        </div>
+        <div className="mt-4 flex flex-col gap-4">
+          {/* we need to map through the topPlays: */}
+          {topPlays?.map((song, i) => (
+            <TopChartCard song={song} i={i} key={song.key} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
