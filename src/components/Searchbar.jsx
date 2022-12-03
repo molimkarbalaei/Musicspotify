116bbcbf8db2f92917e035a8d6dfca699c135eb9
molim <mohade3.karbalaei@gmail.com>
2022-12-03 16:01:40 +0100

    add search bar and style it.

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,33 @@
+// lastly we are focusing on the search bar:
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
+
 const Searchbar = () => (
-  <div>Loader</div>
+  // add search bar:
+  <form
+    autoComplete="off"
+    className="p-2 text-gray-400 focus-within:text-gray-600"
+  >
+    <label htmlFor="search-field" className="sr-only">
+      Are you Searching a song❓
+    </label>
+    <div className="flex flex-row justify-start items-center ">
+      {/* insert a search icon */}
+      <FiSearch className="w-5 h-5 ml-4" />
+      {/* add a search input field */}
+      <input
+        name="search-field"
+        placeholder="Search"
+        autoComplete="off"
+        id="search-field"
+        type="search"
+        value=""
+        onChange={() => {}}
+        className=" flex-1 bg-transparent border-none outline-none placeholder-bg-gray-500 text-base text-white p-4"
+      />
+    </div>
+  </form>
 );
 
 export default Searchbar;
