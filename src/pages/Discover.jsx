ca84473e6772449319f1daeaa1cf0e1d945c77af
molim <mohade3.karbalaei@gmail.com>
2022-12-03 16:30:34 +0100

    add genreListId & genreTitle

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,21 +5,36 @@ import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 // 5- which song is currently active:
 import { useSelector, useDispatch } from "react-redux";
+// lastly later we import :
+import { selectGenreListId } from "../redux/features/playerSlice";
+// add later for different genres:
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore";
+
+//! we use Distpach to modify the state of the store: IMPORTANT*****
+//** distpach => distpach an action-> get that specific genre
+//** selector -> fetch that modified state from the store
 
 const Discover = () => {
   // 5- which song is currently active: we use this hooks:
   const dispatch = useDispatch();
   // To use specific data from the store, we use the useSelector hook: cake
   //cake => cake.vanila
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { activeSong, isPlaying, genreListId } = useSelector(
+    (state) => state.player
+  );
 
   // we can call it(useGetTopChartsQuery => api) like other hooks:
   // we get back 3 different things:
   // a- data: the data we get back from the api => result of api call
   // b- isFetching: boolean => if we are fetching data or not
   // c- error: if there is an error
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = "Pop";
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  );
+  //do it for all genres:
+  //value is genre id
+  // const genreTitle = "Pop";
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
   // 4- for lodaidng:
   if (isFetching) {
@@ -36,9 +51,12 @@ const Discover = () => {
           Discover {genreTitle}
         </h2>
         {/* 1- for choosing: */}
+        {/*  if we want this one works  we have to use distpach functionality*/}
         <select
-          onChange={() => {}}
-          value=""
+          onChange={(e) => {
+            dispatch(selectGenreListId(e.target.value));
+          }}
+          value={genreListId || "pop"}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5 "
         >
           {/* map from genres: */}
